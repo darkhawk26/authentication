be4884123ae89c1d6686d79ceb3fdf6b81e37234
USER <USER@DESKTOP-43KGHVD>
2024-12-02 09:42:30 +0200

    Add project files.

---
+++ AuthenAndAutho/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize(Roles = "Admin")]
+public class AdminController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult ManageUsers()
+    {
+        // Логика за управление на потребителите
+        return View();
+    }
+
+    public IActionResult ManageRoles()
+    {
+        // Логика за управление на ролите
+        return View();
+    }
+}
